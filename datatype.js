e600b1b593cac6ddf574ad7af3fcb7f9d4923426
VanshAbes <vansh.25b01010066@abes.ac.in>
2026-08-19 10:02:10 +0530

    DataTypes

---
+++ datatype.js
@@ -0,0 +1,17 @@
+let a=10;
+console.log(typeof(a)); // number
+
+let name="bikas";
+console.log(typeof(name)); // string
+
+let n;
+console.log(typeof(n)); //undefine
+
+let y=NULL;
+console.log(typeof(y)); //NULL (BY ERROR its object)
+
+
+const m={
+name:"vikas"
+}
+console.log(typeof(m));
