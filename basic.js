1c3d2784dc1ff0f2b442d286e2a619c29164bd3c
VanshAbes <vansh.25b01010066@abes.ac.in>
2026-08-19 09:33:16 +0530

    DAY-1

---
+++ basic.js
@@ -0,0 +1,22 @@
+//var : used in older vesion and in local scope
+
+// var name = "Alex";
+// name = "Odin";
+// console.log(name);
+
+//let: work as a global scope
+
+// let name = "Alex";
+// name = "Odin";
+// console.log(name);
+
+//const: make constant in whole program
+
+// const name = "Alex";
+// console.log(name);
+
+const arr1 = ['Mango', 'Watermelon', 'Banana'];
+const arr2 = ['Apple', 'Muskmelon', 'Grapes'];
+const newarr = [...arr1, ...arr2];
+console.log(newarr);
+
---
+++ operator.js
@@ -0,0 +1,9 @@
+const x = 5;
+const y = 10;
+
+// Arithmetic Operators
+console.log(x + y); // Addition
+console.log(x - y); // Subtraction
+console.log(x * y); // Multiplication
+console.log(x / y); // Division
+console.log(x % y); // Modulus
