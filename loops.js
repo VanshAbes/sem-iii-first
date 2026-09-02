8078704a7ce92b256467b14735db9b8897414879
VanshAbes <vansh.25b01010066@abes.ac.in>
2026-09-02 09:06:41 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,40 @@
+//loops: these are used to iterate a statement over a period of time
+
+//Types of Loop:
+//1. for loop
+
+// for (initization; condition; increment / decrement) 
+// {
+//     Statement
+// }
+
+
+// 2. while loop
+
+// initialization
+// while(condition)
+// {
+//     statement
+//     increment/decrement
+// }
+
+
+//3. do while loop
+
+//initialize
+//do
+// {
+//     statement
+//     increment/decrement
+// }while(condition);
+
+// let x = 15
+// let y = (x++, x+2)
+// console.log(y)
+
+// let a = [10,20,30,40,50]
+// for(let i = 0,j = a.length - 1; i<j; i++, j--){
+//     t = a[i];
+//     console.log(t);
+// }
+
