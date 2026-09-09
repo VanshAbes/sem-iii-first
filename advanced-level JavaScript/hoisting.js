4cf489fb4b07a33a60c63c13991516e31ff11962
VanshAbes <vansh.25b01010066@abes.ac.in>
2026-09-09 09:33:27 +0530

    Advanced JavaScript

---
+++ advanced-level JavaScript/hoisting.js
@@ -0,0 +1,7 @@
+console.log(age);
+var name = "vikas"; // Undefined
+
+console.log(age);
+let age = 20; // reference Error and Temporaral Dead Zone
+
+//Temporal Dead Zone: The line after which the variable(using let/const) is defined till it is called.
