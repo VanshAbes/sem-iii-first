c810b83cb29a215cf77cdd83363ec4f1b1ed7b67
VanshAbes <vansh.25b01010066@abes.ac.in>
2026-09-09 09:56:29 +0530

    Higher Order Function

---
+++ advanced-level JavaScript/Higher Order Function/_map.js
@@ -0,0 +1,13 @@
+//.map
+
+let numbers = [1,2,3,4,5];
+// for(let i = 0; i<5; i++){
+//     console.log(numbers[i]*numbers[i]);
+// }
+
+let squares = numbers.map(function(number){
+    return number*number;
+});
+
+//using arroe function
+
